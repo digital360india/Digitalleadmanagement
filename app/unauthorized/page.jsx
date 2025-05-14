48579c1398c35859510990ded84fafcbb0169383
Yogesh Pal <yogeshpal5049@gmail.com>
2025-05-14 16:40:47 +0530

    add background color and padding for improved visibility

--- app/unauthorized/page.jsx
+++ app/unauthorized/page.jsx
@@ -1,6 +1,6 @@
 export default function UnauthorizedPage() {
   return (
-    <div className="h-screen flex justify-center items-center text-red-500 text-xl">
+    <div className="h-screen bg-gray-500 p-8 flex justify-center items-center text-red-500 text-xl">
       Unauthorized user! Access Login First
     </div>
   );
